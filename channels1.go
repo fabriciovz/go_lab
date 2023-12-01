4d4c48265f6d09919439170f7f0b87116374e33a
Fabricio Bravo Guevara <fabricio.bravo0@gecgithub01.walmart.com>
2023-12-01 12:26:37 -0300

    adding channels sample 1

--- channels1.go
+++ channels1.go
@@ -1,1 +1,35 @@
 package go_lab
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func DoWork() int {
+	time.Sleep(time.Second)
+	return rand.Intn(100)
+}
+
+func MainChain() {
+	dataChan := make(chan int)
+
+	go func() {
+		wg := sync.WaitGroup{}
+		for i := 0; i < 1000; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				result := DoWork()
+				dataChan <- result
+			}()
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	for n := range dataChan {
+		fmt.Printf("n =%d\n", n)
+	}
+}
